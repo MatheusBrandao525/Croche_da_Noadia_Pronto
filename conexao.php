31bc8477240d0764891257c873452d6fdfe2228c
MatheusBrandao525 <77033014+MatheusBrandao525@users.noreply.github.com>
2022-04-09 16:40:35 -0400

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,9 +1,9 @@
 <?php 
 
     $servidor ="localhost";
-    $usuario ="u419134985_Noadia";
-    $senha ="Brandao10@";
-    $banco ="u419134985_crochesNoa";
+    $usuario ="";
+    $senha ="";
+    $banco ="";
 
     $cn = new PDO("mysql:host=$servidor;dbname=$banco", $usuario, $senha);
 
